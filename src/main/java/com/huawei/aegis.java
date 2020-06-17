a4c4321bbb8c7ab9cfef84c2bea1e00893c319e0
wangyun2018 <727764020@qq.com>
2020-06-17 22:24:07 +0800

    修改1

--- src/main/java/com/huawei/aegis.java
+++ src/main/java/com/huawei/aegis.java
@@ -3,6 +3,6 @@ package com.huawei;
 public class aegis {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("hello world2");
+        System.out.println("hello wangyun");
     }
 }
